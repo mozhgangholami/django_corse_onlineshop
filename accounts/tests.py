2b5a1472386813649300d2adfb459f8a8b24fec3
mozhgan gholami <mozhgan gholami>
2025-06-05 13:48:40 -0700

    add signup and login

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
-
-# Create your tests here.
+# from django.test import TestCase
+#
+# from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+# from accounts.models import CustomUser
+#
+#
+# class CustomUserCreationFormTests(TestCase):
+#
+#     def test_valid_form_creates_user(self):
+#         form_data = {
+#             'username': 'mozhgang',
+#             'email': 'mozhgan@test.com',
+#         }
+#
+#     form = CustomUserCreationForm(data=form_data)
+#     self.assertTrue(form.is_valid())
+#     user = form.save()
+#     self.assertEqual(user.username, 'mozhgang')
+#     self.assertEqual(user.email, 'mozhgan@test.com')
